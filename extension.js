407b240d4bea87670c003d1a35b21c707518f2c5
acters <Acters@users.noreply.github.com>
2024-08-21 22:55:35 -0700

    Update to GNOME 46, change to Async, use IP command
    This works in GNOME 46.
    Made changes to switch from `GLib.spawn_command_line_sync` to `Gio.Subprocess` because `GLib.spawn_command_line_async` is not properly handled in GNOME Shell extensions. This should now be resistant to freezing and will silently fail instead. (all errors are caught and logged to console, and status bar will say "None" or "Error...")
    Will now use `IP` command to get interface addresses instead, and updated parsing the output to match.

--- extension.js
+++ extension.js
@@ -5,106 +5,159 @@ import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 import St from 'gi://St';
 import Clutter from 'gi://Clutter';
 import GLib from 'gi://GLib';
+import Gio from 'gi://Gio';
 import GObject from 'gi://GObject';
 import {Extension} from 'resource:///org/gnome/shell/extensions/extension.js';
 
-// Start with WAN address as default
-var type=4;
+// Start with tun0 address as default
+var type=1;
 
-function _get_tun0() {
-    // Run ifconfig and pull the ip address for tun0 or vpn0
-    var command_output_bytes = GLib.spawn_command_line_sync('/bin/bash -c "ifconfig vpn0 || ifconfig tun0"')[1];
-    var command_output_string = '';
+function executeShellCommand(argv) {
+    let flags = (Gio.SubprocessFlags.STDOUT_PIPE |
+                 Gio.SubprocessFlags.STDERR_PIPE);
 
-    for (var i = 0; i < command_output_bytes.length; ++i){
-        var current_character = String.fromCharCode(command_output_bytes[i]);
-        command_output_string += current_character;
+    let proc = Gio.Subprocess.new(argv, flags);
+
+    return new Promise((resolve, reject) => {
+        proc.communicate_utf8_async(null, null, (proc, res) => {
+            try {
+                let [, stdout, stderr] = proc.communicate_utf8_finish(res);
+                let status = proc.get_exit_status();
+
+                if (status !== 0) {
+                    throw new Gio.IOErrorEnum({
+                        code: Gio.io_error_from_errno(status),
+                        message: stderr ? stderr.trim() : GLib.strerror(status)
+                    });
+                }
+
+                resolve(stdout.trim());
+            } catch (e) {
+                reject(e);
+                return " None";
+            }
+        });
+    });
+}
+
+async function _get_VPN_address(Panel_Object) {
+    var vpn0_result = ' None';
+    var tun0_result = ' None';
+    // pull the ip address for tun0 and vpn0
+    log("getting vpn0");
+    try {
+         vpn0_result = await executeShellCommand(['ip', '-brief', '-4', 'addr', 'show', 'dev', 'vpn0']);
+         
+         // Success
+         log("vpn0 Success");
+    } catch (e) {
+        // Error
+        logError(e);
+        vpn0_result = ' None';
+    }
+    log("getting tun0");
+    try {
+         tun0_result = await executeShellCommand(['ip', '-brief', '-4', 'addr', 'show', 'dev', 'tun0']);
+         
+         // Success
+         log("tun0 Success");
+    } catch (e) {
+        // Error
+        logError(e);
+        tun0_result = ' None';
     }
 
-    var Re = new RegExp(/inet [^ ]+/g);
-    var matches = command_output_string.match(Re);
+    var Re = new RegExp(/ ([0-9]+([.][0-9]+)+)/g);
+    var matches = vpn0_result.match(Re);
+    var vpn0IpAddress;
+    if (matches) {
+        vpn0IpAddress = matches[0].split(' ')[1];
+    } else {
+        vpn0IpAddress = ' None';
+    }
+    matches = tun0_result.match(Re);
     var tun0IpAddress;
     if (matches) {
         tun0IpAddress = matches[0].split(' ')[1];
     } else {
-        tun0IpAddress = '';
+        tun0IpAddress = ' None';
     }
-    return tun0IpAddress;
+    var VPNIpAddress = ' None';
+    // checks both interfaces if they are set then it will append both, else it will choose one; if both are none then it is None.
+    VPNIpAddress = vpn0IpAddress.includes("None") ? (tun0IpAddress.includes("None") ? " None" : tun0IpAddress) : (tun0IpAddress.includes("None") ? vpn0IpAddress : (vpn0IpAddress + ", "+tun0IpAddress))
+    Panel_Object.buttonText.set_text("VPN: " + VPNIpAddress);
 }
 
-function _get_lan_ip4() {
+async function _get_lan_ip4(Panel_Object) {
     // Ask the IP stack what route would be used to reach 1.1.1.1 (Cloudflare DNS)
     // Specifically, what src would be used for the 1st hop?
-    var command_output_bytes = GLib.spawn_command_line_sync('ip route get 1.1.1.1')[1];
-    var command_output_string = '';
-
-    for (var current_character_index = 0;
-        current_character_index < command_output_bytes.length;
-        ++current_character_index)
-    {
-        var current_character = String.fromCharCode(command_output_bytes[current_character_index]);
-        command_output_string += current_character;
+    var command_output = ' None';
+    try {
+         command_output = await executeShellCommand(['ip', '-brief', '-4', 'addr', 'show', 'dev', 'eth0']);
+         
+         // Success
+         log("Lan Success");
+    } catch (e) {
+        // Error
+        logError(e);
+        command_output = ' None';
     }
 
     // Output of the "ip route" command will be a string
-    // " ... src 1.2.3.4 ..."
-    // So basically we want the next token (word) immediately after the "src"
-    // word, and nothing else. This is considerd our LAN IP address.
-    var Re = new RegExp(/src [^ ]+/g);
-    var matches = command_output_string.match(Re);
+    // "eth0             UP             192.168.100.100/24"
+    var Re = new RegExp(/ ([0-9]+([.][0-9]+)+)/g);
+    var matches = command_output.match(Re);
     var lanIpAddress;
     if (matches) {
-        lanIpAddress = matches[0].split(' ')[1];
+        lanIpAddress = matches[0];
     } else {
-        lanIpAddress = '';
+        lanIpAddress = ' None';
     }
 
-    return lanIpAddress;
+    Panel_Object.buttonText.set_text("LAN: " + lanIpAddress);
 }
 
-function _get_lan_ip6() {
-    // Ask the IP stack what route would be used to reach 2001:: (random ipv6 address)
-    // Specifically, what src would be used for the 1st hop?
-    var command_output_bytes = GLib.spawn_command_line_sync('ip route get 2001::')[1];
-    var command_output_string = '';
-
-    for (var current_character_index = 0;
-        current_character_index < command_output_bytes.length;
-        ++current_character_index)
-    {
-        var current_character = String.fromCharCode(command_output_bytes[current_character_index]);
-        command_output_string += current_character;
+async function _get_lan_ip6(Panel_Object) {
+    var command_output = ' None';
+    try {
+         command_output = await executeShellCommand(['ip', '-brief', '-6', 'addr', 'show', 'dev', 'eth0']);
+         
+         // Success
+         log("Success");
+    } catch (e) {
+        // Error
+        logError(e);
+        command_output = ' None';
     }
 
-    // Output of the "ip route" command will be a string
-    // " ... src 2001:xxx:yyy:..."
-    // So basically we want the next token (word) immediately after the "src"
-    // word, and nothing else. This is considerd our LAN IP address.
-    var Re = new RegExp(/src [^ ]+/g);
-    var matches = command_output_string.match(Re);
-    var lanIpAddress;
+    // Output of the "ip" command will be a string
+    // "eth0             UP             fa4e::add:7e55:1976:1/64"
+    var Re = new RegExp(/ ([:A-Za-z0-9]+(:[A-Za-z0-9]+)+)/g);
+    var matches = command_output.match(Re);
+    var lanIpv6Address;
     if (matches) {
-        lanIpAddress = matches[0].split(' ')[1];
+        lanIpv6Address = matches[0];
     } else {
-        lanIpAddress = '';
+        lanIpv6Address = ' None';
     }
-    return lanIpAddress;
+    Panel_Object.buttonText.set_text("IPv6: "+ lanIpv6Address)
 }
 
-function _get_wan_ip4() {
+async function _get_wan_ip4(Panel_Object) {
     // Use the google dns servers to find the publip ip address used for requests
     // Force a ipv4 conection, because ipv6 won't be NAT'ed
-    var command_output_bytes = GLib.spawn_command_line_sync('dig TXT +short o-o.myaddr.l.google.com @ns1.google.com -4')[1];
-    var command_output_string = '';
-
-    for (var current_character_index = 0;
-        current_character_index < command_output_bytes.length;
-        ++current_character_index)
-    {
-        var current_character = String.fromCharCode(command_output_bytes[current_character_index]);
-        command_output_string += current_character;
+    var command_output = ' None';
+    try {
+         command_output = await executeShellCommand(['dig', 'TXT', '+short', 'o-o.myaddr.l.google.com', '@ns1.google.com', '-4']);
+         
+         // Success
+         log("Success");
+    } catch (e) {
+        // Error
+        logError(e);
+        command_output = ' None';
     }
-    command_output_string=command_output_string.replace('"','').replace('"','').replace('\n','');
+    var command_output_string = command_output.replace('"','').replace('"','').replace('\n','');
     // Validate the result looks like an ipv4 address
     var Re = new RegExp(/.*\..*\..*\..*/g);
     var matches = command_output_string.match(Re);
@@ -112,9 +165,9 @@ function _get_wan_ip4() {
     if (matches) {
         wanIpAddress = command_output_string;
     } else {
-        wanIpAddress = '';
+        wanIpAddress = ' None';
     }
-    return wanIpAddress;
+    Panel_Object.buttonText.set_text("WAN: " + wanIpAddress);
 }
 
 class AllIPAddressIndicator extends PanelMenu.Button{
@@ -158,14 +211,27 @@ class AllIPAddressIndicator extends PanelMenu.Button{
         }
         this._timeout = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, refreshTime, () => {this._updateLabel();});
         // Show the right format. 0 = WAN, 4 = IPv4, 6=IPv6
+        // Do it asynchronously
         if (type===4) {
-            this.buttonText.set_text("LAN: "+_get_lan_ip4());
+          _get_lan_ip4(this).catch((e) => {
+            logError(e);
+            this.buttonText.set_text("LAN: Error...");
+          });
         } else if (type===0) {
-            this.buttonText.set_text("WAN: "+_get_wan_ip4());
+          _get_wan_ip4(this).catch((e) => {
+            logError(e);
+            this.buttonText.set_text("WAN: Error...");
+          });
         } else if (type===6){
-            this.buttonText.set_text("IP6: "+_get_lan_ip6());
+          _get_lan_ip6(this).catch((e) => {
+            logError(e);
+            this.buttonText.set_text("IPv6: Error...");
+          });
         } else {
-            this.buttonText.set_text("VPN: "+_get_tun0());
+          _get_VPN_address(this).catch((e) => {
+            logError(e);
+            this.buttonText.set_text("VPN: Error...");
+          });
         }
     }
 
